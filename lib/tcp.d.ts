89bb715d88567eb0544ed04755cc39c5c47500e0
Iñaki Baz Castillo <ibc@aliax.net>
2024-01-03 19:24:02 +0100

    foo

---
+++ lib/tcp.d.ts
@@ -0,0 +1,6 @@
+export declare function reserve({ ip, port, family }: {
+    ip: string;
+    port: number;
+    family: 4 | 6;
+}): Promise<void>;
+//# sourceMappingURL=tcp.d.ts.map
---
+++ lib/udp.d.ts
@@ -0,0 +1,6 @@
+export declare function reserve({ ip, port, family }: {
+    ip: string;
+    port: number;
+    family: 4 | 6;
+}): Promise<void>;
+//# sourceMappingURL=udp.d.ts.map
